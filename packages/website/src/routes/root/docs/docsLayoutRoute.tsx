fa4471e7946cb26bc046d1a651e096efd98ed855
&mile <dev@emilesabatier.com>
2026-07-17 00:52:29 +0200

    Remove getParentRoute from docsLayoutRoute, let addChildren handle it

--- packages/website/src/routes/root/docs/docsLayoutRoute.tsx
+++ packages/website/src/routes/root/docs/docsLayoutRoute.tsx
@@ -1,9 +1,7 @@
 import { createRoute } from "@tanstack/react-router"
-import { rootLayoutRoute } from "../../rootLayoutRoute.js"
 import { DocsLayout } from "../../../features/docs/DocsLayout.js"
 
 export const docsLayoutRoute = createRoute({
-    getParentRoute: () => rootLayoutRoute,
     path: "/documentation",
     component: () => <DocsLayout />,
 })
