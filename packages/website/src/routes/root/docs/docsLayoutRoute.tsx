841b5b77b906ba69e8d227527e8cd1229613ffd0
&mile <dev@emilesabatier.com>
2026-07-16 19:44:03 +0200

    Remove beforeLoad from docsLayoutRoute to test if it was blocking route resolution

--- packages/website/src/routes/root/docs/docsLayoutRoute.tsx
+++ packages/website/src/routes/root/docs/docsLayoutRoute.tsx
@@ -4,11 +4,5 @@ import { rootLayoutRoute } from "../../rootLayoutRoute.js"
 export const docsLayoutRoute = createRoute({
     getParentRoute: () => rootLayoutRoute,
     path: "/documentation",
-    beforeLoad: () => ({
-        title: "Documentation",
-        section: "Documentation",
-        description:
-            "Documentation complète d'Arrhes : guide d'utilisation, cours de comptabilité, référence API et informations générales.",
-    }),
     component: () => <div style={{ padding: "2rem" }}><h1>DOCS WORKS</h1><Outlet /></div>,
 })
