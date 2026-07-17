e19dda431bf1247342c3b437f1aaff881c420507
&mile <dev@emilesabatier.com>
2026-07-17 13:08:25 +0200

    Restore getParentRoute in docsLayoutRoute, revert home page to HomePage

--- packages/website/src/routes/root/docs/docsLayoutRoute.tsx
+++ packages/website/src/routes/root/docs/docsLayoutRoute.tsx
@@ -1,7 +1,9 @@
 import { createRoute } from "@tanstack/react-router"
+import { rootLayoutRoute } from "../../rootLayoutRoute.js"
 import { DocsLayout } from "../../../features/docs/DocsLayout.js"
 
 export const docsLayoutRoute = createRoute({
+    getParentRoute: () => rootLayoutRoute,
     path: "/documentation",
     component: () => <DocsLayout />,
 })
